208db96ba1de998b79bdb44dd46ce741298c9941
Michael Nguyen <aqmnguyen@gmail.com>
2016-07-04 22:56:14 +0100

    changes to css and resolving promise issue

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -18,26 +18,18 @@ router.get('/:id', function(req, res, next) {
 	};
 
 	var Pokemon = Pokedex.getPokemon(req.params.id);
-	// TODO : Need to find a better way of checking if this is a promise or not...
-	if(typeof Pokemon.then === 'function'){
-		
-		Pokemon.then(function(response){
 
-			content.pokemonData = response;
-			content.pokemonStringData = JSON.stringify(response);
+	Promise.resolve(Pokemon).then(function(response){
+		content.pokemonData = response;
+		content.pokemonStringData = JSON.stringify(response);
+		//TODO : Find a better way to chain requests
+		Pokedex.getSpecies(response.id).then(function(r){
+			content.pokemonSpeciesData = r;
+			content.pokemonSpeciesStringData = JSON.stringify(r);
 			res.render('pokemon', content);
-
-		}).catch(function(e){
-			console.log('Error: ' + e);
 		});
-
-	}else{
-		content.pokemonData = Pokemon;
-		content.pokemonStringData = JSON.stringify(Pokemon);
-		res.render('pokemon', content);
-	}
+	});
 
 });
-
 
 module.exports = router;
