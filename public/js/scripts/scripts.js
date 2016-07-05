933b77076516709c08040521ae7f252a2d34cfa2
Craig Clayton <craig_clayton@hotmail.co.uk>
2016-07-05 23:47:54 +0100

    Modified Dexter <3, replaced api calls for images with local

--- public/js/scripts/scripts.js
+++ public/js/scripts/scripts.js
@@ -3,12 +3,14 @@ var URL_PARAMS = {};
 
 $(document).ready(function(){
 	getQS(URL_PARAMS);
-	window.pokedexText = speciesPokeApi.flavor_text_entries[1].flavor_text;
 	console.log(speciesPokeApi);
-	console.log(pokedexText);
-	speak();
+	$('#dexter').click(function(){
+		var pokedexText = speciesPokeApi.flavor_text_entries[1].flavor_text;
+		console.log(pokedexText);
+		speak(pokedexText);
+	});
 });
 
-var speak = function() {
-  responsiveVoice.speak(window.pokedexText, "UK English Male");
+var speak = function(pokedexText) {
+  responsiveVoice.speak(pokedexText, "UK English Male");
       };
