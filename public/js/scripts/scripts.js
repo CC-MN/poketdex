15bbc5966df642b2dd818a0c9a410587eafe2269
Michael Nguyen <aqmnguyen@gmail.com>
2016-07-05 23:57:42 +0100

    making styles camalCase and adding local version of ResponsiveVoice

--- public/js/scripts/scripts.js
+++ public/js/scripts/scripts.js
@@ -3,14 +3,16 @@ var URL_PARAMS = {};
 
 $(document).ready(function(){
 	getQS(URL_PARAMS);
-	console.log(speciesPokeApi);
+
+	//binding dexter click event
 	$('#dexter').click(function(){
 		var pokedexText = speciesPokeApi.flavor_text_entries[1].flavor_text;
 		console.log(pokedexText);
 		speak(pokedexText);
 	});
+
 });
 
 var speak = function(pokedexText) {
-  responsiveVoice.speak(pokedexText, "UK English Male");
-      };
+	responsiveVoice.speak(pokedexText, "UK English Male");
+};
