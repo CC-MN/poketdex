58c407257303e6190981b9dfa23d0c50722b974f
Craig Clayton <craig_clayton@hotmail.co.uk>
2016-07-05 23:20:19 +0100

    Added Dexter

--- public/js/scripts/scripts.js
+++ public/js/scripts/scripts.js
@@ -1,5 +1,14 @@
 var URL_PARAMS = {};
 
+
 $(document).ready(function(){
 	getQS(URL_PARAMS);
+	window.pokedexText = speciesPokeApi.flavor_text_entries[1].flavor_text;
+	console.log(speciesPokeApi);
+	console.log(pokedexText);
+	speak();
 });
+
+var speak = function() {
+  responsiveVoice.speak(window.pokedexText, "UK English Male");
+      };
