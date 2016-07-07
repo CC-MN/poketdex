9181fd78acb752859fc13a52ab2036d76a5a8a47
Craig Clayton <craig_clayton@hotmail.co.uk>
2016-07-07 23:31:16 +0100

    added for loop to identify correct flavour text

--- public/js/scripts/scripts.js
+++ public/js/scripts/scripts.js
@@ -4,11 +4,21 @@ var URL_PARAMS = {};
 $(document).ready(function(){
 	getQS(URL_PARAMS);
 
-	//binding dexter click event
+	console.log("starting loop");
+	console.log(responsePokemonSpecies.flavor_text_entries);
+	for (var i = 0; i < 5; i ++) {
+		// console.log("this actually works");
+		if (responsePokemonSpecies.flavor_text_entries[i].language.name === "en") {
+			window.pokedexText = responsePokemonSpecies.flavor_text_entries[i].flavor_text
+			console.log("you got: " + responsePokemonSpecies.flavor_text_entries[i].language.name);
+		}
+	};
+
+	// binding dexter click event
 	$('#dexter').click(function(){
-		var pokedexText = responsePokemonSpecies.flavor_text_entries[1].flavor_text;
-		console.log(pokedexText);
-		speak(pokedexText);
+		// var pokedexText = responsePokemonSpecies.flavor_text_entries[1].flavor_text;
+		// console.log(pokedexText);
+		speak(window.pokedexText);
 	});
 
 });
