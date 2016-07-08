a62931c42a6a1b535e29361c8ad3005bfd60f964
Craig Clayton <craig_clayton@hotmail.co.uk>
2016-07-08 16:59:30 +0100

    Modified top nav inputs

--- public/js/scripts/scripts.js
+++ public/js/scripts/scripts.js
@@ -4,12 +4,14 @@ var URL_PARAMS = {};
 $(document).ready(function(){
 	getQS(URL_PARAMS);
 
-	for (var i = 0; i < 5; i ++) {
-		if (responsePokemonSpecies.flavor_text_entries[i].language.name === "en") {
+	for (var i = 0; i < responsePokemonSpecies.flavor_text_entries.length; i ++) {
+		if (responsePokemonSpecies.flavor_text_entries[i].language.name === "en" && responsePokemonSpecies.flavor_text_entries[i].version.name === "alpha-sapphire") {
 			window.pokedexText = responsePokemonSpecies.flavor_text_entries[i].flavor_text
-			console.log("you got: " + responsePokemonSpecies.flavor_text_entries[i].language.name);
+			// console.log("you got: " + responsePokemonSpecies.flavor_text_entries[i].language.name);
+			// console.log("you got: " + responsePokemonSpecies.flavor_text_entries[i].flavor_text);
 		}
 	};
+
 
 	// binding dexter click event
 	$('#dexter').click(function(){
@@ -17,6 +19,8 @@ $(document).ready(function(){
 		// console.log(pokedexText);
 		speak(window.pokedexText);
 	});
+
+	$("#pokedexList").val(responsePokemon.id);
 
 });
 
@@ -33,3 +37,10 @@ function openNav() {
 function closeNav() {
 	$('#mySidenav').css('width','0px');
 }
+
+function changePokemon(pokemonID) {
+	window.location = "./" + pokemonID;
+}
+
+//get selected option and set text
+//$( "#myselect option:selected" ).text();
