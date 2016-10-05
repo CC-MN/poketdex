bdd96ce11735f8c850dd80eaacd605ea23f19fc5
Craig Clayton <craig_clayton@hotmail.co.uk>
2016-10-05 21:27:49 +0100

    testing overlay function

--- public/js/scripts/util.js
+++ public/js/scripts/util.js
@@ -84,7 +84,7 @@ function showSearch(){
 
 // Call Page Change Animation
 function loadOverlay(){
-  alert('load overlay');
+  // alert('load overlay');
   console.log('loading new page...');
   $('#searchContainer').addClass('hidden');
   $('#loadingDiv').removeClass('hidden');
